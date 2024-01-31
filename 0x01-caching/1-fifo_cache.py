23c5ff06361b1a04b0f0cc7053718cec4d7bc0ec
a-dabour <ah910905@gmail.com>
2024-01-31 10:18:33 +0200

    edit

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python3
 """FIFOCache as a caching system"""
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class FIFOCache(BaseCaching):
     """the caching system"""
     def __init__(self):
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """"put an item for a key in the cache using FIFO algorithm"""
         if key is not None and item is not None:
-            if key in self.cache_data.keys():
-                self.cache_data[key] = item
-                return
-            elif len(self.cache_data) == self.MAX_ITEMS:
-                # mykeys = list(self.cache_data.key())
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                # mykeys = list(self.cache_data.keys())
                 # del self.cache_data[mykeys[0]]
                 firstItem = next(iter(self.cache_data))
                 del self.cache_data[firstItem]
-                print(f"DISCARD: {firstItem}\n")
-            self.cache_data[key] = item
+                print("DISCARD:", firstItem)
 
     def get(self, key):
         """get the value of the key in the cache"""
-        if key is None or key not in self.cache_data.key():
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)
